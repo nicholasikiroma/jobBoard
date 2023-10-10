04cc6f7fb6645b109c09d397e7f899ccb8a23f99
Nicholas <nicholasikiroma@gmail.com>
2023-10-10 21:25:17 +0100

    handle uncaught exceptions

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import app from "./app/index.js";
 import logger from "./app/config/logger.js";
 import pkg from "./app/config/baseConfigs.cjs";
+import { errorHandler } from "./app/middlewares/errorHandler.js";
 const { port } = pkg;
+
+process.on("uncaughtException", (error) => {
+  errorHandler.handleError(error);
+  if (!errorHandler.isTrustedError(error)) {
+    process.exit(1);
+  }
+});
 
 app.listen(port, () => {
   logger.info(`Server running on ${port}`);
